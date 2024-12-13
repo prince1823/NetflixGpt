9696c7e914155c9a942a3200de274eb574ce97c0
Your Name <prince40saxena@gmail.com>
2024-12-13 16:07:52 +0530

    Alright

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,8 +1,8 @@
 import OpenAI from 'openai';
-
+import chatgptAPI from "../api_key";
 
 const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: chatgptAPI,
   dangerouslyAllowBrowser:true,
 });
 
